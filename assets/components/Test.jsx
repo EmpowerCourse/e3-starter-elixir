825cfcd8aa0332ce73430a6b347b74692a305ec5
Melissa Capps <Melissa.Capps@podium.com>
2023-08-29 13:19:11 -0400

    some helpers

--- assets/components/Test.jsx
+++ assets/components/Test.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+function handleSubmit(e) {
+  e.preventDefault();
+  api.post("add_song", {
+      song_name: username,
+      artist: password,
+    })
+    .then((response) => {
+      if (response.data.success) {
+        setMessage({flavor: "success", text: "Log-In Successful!"});
+      } else {
+        setMessage({flavor: "danger", text: response.data.message});
+      }
+    }).catch(helpers.catchHandler);
+}
 function Test() {
   return <div>This is a component</div>;
 }
---
+++ assets/utils/index.js
@@ -0,0 +1,6 @@
+export { default as api } from './api';
+export { default as constants } from './constants';
+export { default as dates } from './dates';
+export { default as filters } from './filters';
+export { default as helpers } from './helpers';
+export { default as socket } from './socket';
